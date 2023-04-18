502ce9c3d75ae017d8ab1e97a0de5fbbbbaaaa59
rafael E Carlos <carlosrafaelgit@gmail.com>
2023-04-18 08:56:26 -0300

    Encapsulamento da classe Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,12 +1,73 @@
-public class Livro {
+import java.util.IllegalFormatCodePointException;
 
-    String nome;
-    String resumo;
-    double preco;
-    int paginas;
-    String autor;
-    Editora editora;
-    TipoCapaEnum tipoCapa;
+public class Livro {
+    // Encapsulamento = deixar atributos e métodos como privados
+    private String nome;
+    private String resumo;
+    private double preco;
+    private int paginas;
+    private String autor;
+    private Editora editora;
+    private TipoCapaEnum tipoCapa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        if (paginas <= 0)
+            System.out.println("Páginas Inválidas");
+        else
+            this.paginas = paginas;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public TipoCapaEnum getTipoCapa() {
+        return tipoCapa;
+    }
+
+    public void setTipoCapa(TipoCapaEnum tipoCapa) {
+        this.tipoCapa = tipoCapa;
+    }
 
     public Livro(String nome, double preco) {
         this.nome = nome; // this é referencia aos atributos
@@ -20,14 +81,19 @@ public class Livro {
         System.out.println("Preço: " + preco);
         System.out.println("Páginas: " + paginas);
         System.out.println("Autor: " + autor);
-        System.out.printf("Tipo de capa: " + tipoCapa);
+        System.out.println("Tipo de capa: " + tipoCapa);
         if (temEditora())
             System.out.println("Editora: " + editora.nome);
         System.out.println("-------");
     }
 
     void aplicarDesconto(double percentual) {
-        preco = preco - (preco * percentual);
+        if (percentual > 0.3){
+            System.out.println("Desconto não permitido");
+        }
+        else {
+            preco = preco - (preco * percentual);
+        }
     }
 
     boolean temEditora() {
--- src/LivroTeste.java
+++ src/LivroTeste.java
@@ -4,30 +4,30 @@ public class LivroTeste {
 
         Livro meuLivro = new Livro("O hobbit", 9.99);
         // meuLivro.nome = "O hobbit";
-        meuLivro.paginas = 521;
-        meuLivro.resumo = "bla bla bla bla bla";
-        meuLivro.autor = "J. R. Tolkien";
+        meuLivro.setPaginas(521);
+        meuLivro.setResumo("bla bla bla bla bla");
+        meuLivro.setAutor("J. R. Tolkien");
 
         Editora editora1 = new Editora();
         //editora1.nome = "Editora Erica";
         editora1.email = "edErica@gmai.com.br";
-        meuLivro.editora = editora1;
+        meuLivro.setEditora(editora1);
 
-        meuLivro.aplicarDesconto(0.1);
+        meuLivro.aplicarDesconto(0.5);
 
         meuLivro.exibirDados();
 
         Livro livroFavorito = new Livro("O Código Da Vinci",27.90);
         // livroFavorito.nome = "O Código Da Vinci";
-        livroFavorito.resumo = "bla bla bla bla bla";
-        livroFavorito.paginas = 500;
-        livroFavorito.autor = "Dan Brown";
-        livroFavorito.tipoCapa = TipoCapaEnum.PERSONALIZADA;
+        livroFavorito.setResumo("bla bla bla bla bla");
+        livroFavorito.setPaginas(500);
+        livroFavorito.setAutor("Dan Brown");
+        livroFavorito.setTipoCapa(TipoCapaEnum.PERSONALIZADA);
 
         Editora editora2 = new Editora();
         //editora1.nome = "Editora Erica";
         editora2.email = "edErica@gmai.com.br";
-        livroFavorito.editora = editora2;
+        livroFavorito.setEditora(editora2);
 
         livroFavorito.aplicarDesconto(0.25);
 
